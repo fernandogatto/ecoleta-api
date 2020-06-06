fd2e336edce682a2274f8306efa56eccc0769f6d
fernandogatto <fernandogatto17@gmail.com>
2020-06-06 14:57:59 -0300

    Config upload

---
+++ src/config/upload.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  tmpFolder: tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    }
+  }),
+}
